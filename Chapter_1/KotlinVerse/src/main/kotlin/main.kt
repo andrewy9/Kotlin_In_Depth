e80f7d84aad3e91d983d22d2abb4b3b4b9bbef12
andrewy9 <andrew.yang993@gmail.com>
2023-05-21 15:29:58 +1200

    Chapter_1 initial commit

---
+++ Chapter_1/KotlinVerse/src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Hello, World")
+}
+
+/**
+ * Another way - this was the only acceptable entry point before 1.3
+fun main(args: Array<String>) {
+println("Hello World")
+}
+ */
